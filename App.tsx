86b7d8fda68e65aaf6088125b185421237aff370
Giovani <giakomogcs@gmail.com>
2024-08-26 21:29:54 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Routes } from "./src/routes";
+
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
+
+import { CartContextProvider } from "./src/contexts/CartContext";
+import { OneSignal } from "react-native-onesignal";
+
+OneSignal.initialize("285c0979-6c8e-458e-b4ad-e7185febe51e");
+OneSignal.Notifications.requestPermission(true);
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <CartContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </CartContextProvider>
+    </NativeBaseProvider>
+  );
+}
